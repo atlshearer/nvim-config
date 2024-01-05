3e704188cfdce74b92e81d1a69c4395b646da4f2
Alexander Shearer <77137311+atlshearer-vgw@users.noreply.github.com>
2024-01-05 13:48:28 +0800

    feat: Auto preview markdown

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -119,6 +119,9 @@ return {
         build = "cd app && npm install",
         init = function()
             vim.g.mkdp_filetypes = { "markdown" }
+            vim.g.mkdp_auto_start = 1
+            vim.g.mkdp_auto_close = 0
+            vim.g.mkdp_combine_preview = 1
         end,
         ft = { "markdown" }
     }
